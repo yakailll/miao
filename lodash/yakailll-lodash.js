552012da9a06c6b46d3009a90cbf960bd3002299
ykl <yakai.liang@outlook.com>
2025-03-01 12:30:16 +0800

    创建 yakailll-lodash.js

---
+++ lodash/yakailll-lodash.js
@@ -0,0 +1,23 @@
+/*
+var yakailll = function() {
+  function chunk() {
+
+  }
+  function flattem() {
+
+  }
+  function reverse() {
+
+  }
+
+  return (
+    chunk: chumk,
+    flatten: flatten,
+    reverse: reverse,
+  )
+}()
+  表示定义一个名为 "yakailll" 的变量，指向一个函数，函数里面定义了很多函数
+  返回值是一个对象，对象中将函数名作为属性名，属性值是各自的函数
+  最后的 "()" 表示调用函数，即 yakailll 等于 函数运行后返回的那个对象
+  yakailll.chunk() 就可以调用函数内定义的 chunk 函数
+*/
